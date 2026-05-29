1c143e0dbe31e5ed3c7cd48d23d387e107cc86ca
Irina Markova <145789965+MIreneAl@users.noreply.github.com>
2026-05-29 12:00:15 +0300

    Create task_75.sql
    Add solution for task 75

---
+++ task_75.sql
@@ -0,0 +1,11 @@
+Задание #75
+Студенты, рожденные в мае
+Выведите фамилию, имя и дату рождения студентов, кто был рожден в мае.
+Поля в результирующей таблице:
+last_name, first_name, birthday
+
+SELECT last_name,
+       first_name,
+       birthday
+FROM  Student
+WHERE MONTHNAME(birthday) = 'May';
