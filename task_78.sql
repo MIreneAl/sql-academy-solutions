ef6be3896684c410b769671e06fe9985f237b491
Irina Markova <145789965+MIreneAl@users.noreply.github.com>
2026-06-05 12:29:21 +0300

    Create task_78.sql
    Добавлено решение задачи #78
    Пользователи с почтой hotmail.com

---
+++ task_78.sql
@@ -0,0 +1,8 @@
+Задание #78
+Пользователи с почтой hotmail.com
+Выведите всех пользователей с электронной почтой в «hotmail.com»
+Поля в результирующей таблице: *
+
+SELECT *
+FROM Users
+WHERE email LIKE '%@hotmail.com';
