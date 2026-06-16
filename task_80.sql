31510a2924f5c8ad533c5896a0926c9056d495b7
Irina Markova <145789965+MIreneAl@users.noreply.github.com>
2026-06-16 13:30:45 +0300

    Create task_80.sql
    Задание #80
    Создать представление "Verified_Users"

---
+++ task_80.sql
@@ -0,0 +1,10 @@
+Задание #80
+Создать представление "Verified_Users"
+Создайте представление «Verified_Users» с полями id, name и email, которое будет показывает только тех пользователей, у которых подтвержден адрес электронной почты.
+
+CREATE VIEW Verified_Users AS 
+SELECT  id,
+        name,
+        email
+FROM Users
+WHERE email_verified_at IS NOT NULL;
